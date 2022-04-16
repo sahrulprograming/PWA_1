93800c5fba1d3e890e7c3b021f1d1f98483c316d
sahrul <sahrul@gmail.com>
2022-04-16 11:40:59 +0700

    menambah fitur add to home

--- js/app.js
+++ js/app.js
@@ -55,4 +55,37 @@ $(document).ready(function () {
                 console.log('Service worker registration failed, error:', error);
             });
     }
+
+    let deferredPrompt;
+    var divBtnAdd = document.querySelector('#add-to');
+    var btnAdd = document.querySelector('.add-home');
+
+    divBtnAdd.style.display = 'none';
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI notify the user they can add to home screen
+
+        divBtnAdd.style.display = 'block';
+
+        $('.add-home').on('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            divBtnAdd.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice
+                .then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    deferredPrompt = null;
+                });
+        });
+    });
 })
